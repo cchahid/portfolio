2931df033e4d21332a4b071aeeb827ec1691ae7d
cchahid <c.chahidachraf@gmail.com>
2026-01-17 11:39:47 +0100

    chore: set basePath/assetPrefix for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repo = 'portfolio'; // GitHub repo name
+
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  trailingSlash: true
+  trailingSlash: true,
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}`
 };
 
 module.exports = nextConfig;
